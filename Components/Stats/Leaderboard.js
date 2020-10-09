cf4e1afaff80475deb6030e5f711caecd7532302
Paula Ruiz <paularuiz22@gmail.com>
2020-10-09 01:53:56 -0400

    leaderboard functioning on both android and web browser

--- Components/Stats/Leaderboard.js
+++ Components/Stats/Leaderboard.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
-import { View, Text, Button, Alert, Image } from 'react-native';
+import { View, Text, Button, Alert, Image, Dimensions } from 'react-native';
 import { ButtonGroup } from 'react-native-elements';
 import Leaderboard from 'react-native-leaderboard'
 
 /* Implemented by Paula Ruiz for Sprint 3 of the Jelly Bellies' Communiteer project. This code is
  *  heavily based on the open source project, 'react-native-leaderboard' at https://github.com/JoeRoddy/react-native-leaderboard
  */
+
+const windowWidth = Dimensions.get('window').width;
 
 export default class LeaderBoard extends Component {
      state = {
@@ -84,9 +86,9 @@ export default class LeaderBoard extends Component {
                     onPress={(x) => { this.setState({ filter: x }) }}
                     selectedIndex={this.state.filter}
                     buttons={['Global', 'Friends']}
-                    containerStyle={{ height: 30 }} />
+                    containerStyle={{ height: 30, width: windowWidth/3 }} />
             </View>
-        )
+        );
      }
 
     render() {
@@ -126,80 +128,3 @@ const ordinal_suffix_of = (i) => {
     }
     return i + "th";
 }
-
-const DATA = [
-  {
-    name: "We Tu Lo",
-    score: null,
-    iconUrl:
-      "https://st2.depositphotos.com/1006318/5909/v/950/depositphotos_59094043-stock-illustration-profile-icon-male-avatar.jpg"
-  },
-  {
-    name: "Adam Savage",
-    score: 12,
-    iconUrl:
-      "https://www.shareicon.net/data/128x128/2016/09/15/829473_man_512x512.png"
-  },
-  {
-    name: "Derek Black",
-    score: 244,
-    iconUrl: "http://ttsbilisim.com/wp-content/uploads/2014/09/20120807.png"
-  },
-  {
-    name: "Erika White",
-    score: 0,
-    iconUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr27ZFBaclzKcxg2FgJh6xi3Z5-9vP_U1DPcB149bYXxlPKqv-"
-  },
-  {
-    name: "Jimmy John",
-    score: 20,
-    iconUrl: "https://static.witei.com/static/img/profile_pics/avatar4.png"
-  },
-  {
-    name: "Joe Roddy",
-    score: 69,
-    iconUrl: "https://static.witei.com/static/img/profile_pics/avatar4.png"
-  },
-  {
-    name: "Ericka Johannesburg",
-    score: 101,
-    iconUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShPis8NLdplTV1AJx40z-KS8zdgaSPaCfNINLtQ-ENdPvrtMWz"
-  },
-  {
-    name: "Tim Thomas",
-    score: 41,
-    iconUrl: "http://ttsbilisim.com/wp-content/uploads/2014/09/20120807.png"
-  },
-  {
-    name: "John Davis",
-    score: 80,
-    iconUrl:
-      "https://cdn.dribbble.com/users/223408/screenshots/2134810/me-dribbble-size-001-001_1x.png"
-  },
-  {
-    name: "Tina Turner",
-    score: 22,
-    iconUrl:
-      "https://cdn.dribbble.com/users/223408/screenshots/2134810/me-dribbble-size-001-001_1x.png"
-  },
-  {
-    name: "Harry Reynolds",
-    score: null,
-    iconUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsSlzi6GEickw2Ft62IdJTfXWsDFrOIbwXhzddXXt4FvsbNGhp"
-  },
-  {
-    name: "Betty Davis",
-    score: 25,
-    iconUrl:
-      "https://landofblogging.files.wordpress.com/2014/01/bitstripavatarprofilepic.jpeg?w=300&h=300"
-  },
-  {
-    name: "Lauren Leonard",
-    score: 30,
-    iconUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr27ZFBaclzKcxg2FgJh6xi3Z5-9vP_U1DPcB149bYXxlPKqv-"
-  }
-];
