6c738521475542b7e9c2f027aa98b499211ca4d9
Paula Ruiz <paularuiz22@gmail.com>
2020-10-16 04:59:05 -0400

    add package for react-native-firebase, practice adding data to database, NOT working because unable to find android/iOS source files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 function index() {
+
+    const docRef = db.collection('jobs').doc('walkPaula');
+
+    await docRef.set({
+        'volunteer': '',
+        'requestor': 'paularuiz22',
+        'startTime': '15:00',
+        'endTime': '16:00',
+        'date': '10/25/2020',
+        'description': 'Please walk my dog. She is well-behaved and gentle.'
+    }).then(function(docRef) {
+          console.log("Document written with ID: ", docRef.id);
+
+    const gardenClaraRef = db.collection('jobs').doc('gardenClara');
+
+    await gardenClaraRef.set({
+        'volunteer': '',
+        'requestor': 'claraGkennedy',
+        'startTime': '10:00',
+        'endTime': '12:00',
+        'date': '10/21/2020',
+        'description': 'Please mow my grass and weed the flower beds.'
+    });
+
+    const snapshot = await db.collection('jobs').get();
+    snapshot.forEach((doc) => {
+        console.log(doc.id, '=>', doc.data());
+    });
+
 }
 export default index;
