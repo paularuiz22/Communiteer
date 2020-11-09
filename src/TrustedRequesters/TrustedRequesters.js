be67b2a08c3e05bfad0b95b53c88cfb0daf63967
Paula Ruiz <paularuiz22@gmail.com>
2020-11-09 17:33:00 -0500

    integrate active user, displaying correctly

--- src/TrustedRequesters/TrustedRequesters.js
+++ src/TrustedRequesters/TrustedRequesters.js
@@ -4,26 +4,36 @@ import { Text, SafeAreaView, ScrollView, View } from 'react-native';
 import { db } from "../../config";
 import styles from "../../styles";
 import { AuthContext } from "../../AuthContext";
+import { sortBy } from "lodash";
+import userTypes from "../Users/userType"
 
-const activeUserName = 'paularuiz22'
 
-const TrustedUser = ({user: {firstName, lastName, userType, username}}) => {
-  return (
-    <View style={styles.requestor}>
-        <Text style={styles.mediumText}>{firstName} {lastName}</Text>
-        <Text style={styles.mediumText}>{userType}</Text>
-        <Text style={styles.mediumText}>{username}</Text>
-    </View>
-  );
+//const activeUserName = 'paularuiz22';
+var activeUser  = {
+  username: '',
+  trustedUsers: [],
 };
 
-const ActiveUser = ({user: {userType, username}}) => {
-  if (userType == userTypes.REQUESTOR & username == activeUserName) {
-    return null;
-  } else {
-    return null;
+const TrustedUser = ({user: {firstName, lastName, userType, username}}) => {
+  var trusted = false;
+  for(var i = 0; i < activeUser.trustedUsers.length; i++) {
+    if (activeUser.trustedUsers[i] == username) {
+      trusted = true;
+    }
   }
-}
+  if (userType == userTypes.REQUESTOR & trusted) {
+    return (
+      <View style={styles.requestor}>
+          <Text style={styles.mediumText}>{firstName} {lastName}</Text>
+          <Text style={styles.mediumText}>{userType}</Text>
+          <Text style={styles.mediumText}>{username}</Text>
+      </View>
+    );
+    } else {
+      return null;
+    }
+};
+
 
 class TrustedRequestors extends Component {
 
@@ -43,13 +53,22 @@ class TrustedRequestors extends Component {
       let data = querySnapShot.val() ? querySnapShot.val() : {};
       let userItems = {...data};
       this.setState({
-        allUsers: userItems, //sortBy(userItems, 'username'),
+        allUsers: sortBy(userItems, 'username'),
       });
     }); 
   }
 
   render () {
+    let value = this.context;
     let userKeys = Object.keys(this.state.allUsers);
+    var i = 0;
+    for (var i = 0; i < userKeys.length; i++) {
+      var curr = this.state.allUsers[userKeys[i]];
+      if (curr.username == value["username"]) {
+        activeUser.username = curr.username;
+        activeUser.trustedUsers = curr.trustedUsers;
+      }
+    }
     return (
       <SafeAreaView style={styles.container}>
         <Header
@@ -59,16 +78,16 @@ class TrustedRequestors extends Component {
         <ScrollView style={styles.scrollView}> 
         <View>
           {userKeys.length > 0 ? (
-                      userKeys.map(key => (
-                        <TrustedUser
-                          key={key}
-                          id={key}
-                          user={this.state.allUsers[key]}
-                        />
-                      ))
-                    ): (
-                      <Text>No trusted users</Text>
-                    )}
+            userKeys.map(key => (
+              <TrustedUser
+                key={key}
+                id={key}
+                user={this.state.allUsers[key]}
+              />
+            ))
+          ): (
+            <Text>No trusted users</Text>
+          )}
         </View>
         </ScrollView>
       </SafeAreaView>
